4286f1772405cd1b53a5cd2954ed61ef26f1c995
gabrielbugarelli <gabrielfonsecaoficial@gmail.com>
2023-11-01 16:55:30 -0400

    feat[backend]: authGuard routes implmentation

--- apps/backend/src/modules/users/users.controller.ts
+++ apps/backend/src/modules/users/users.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { AuthGuard } from '../auth/auth.guard';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Get('me')
+  @UseGuards(AuthGuard)
+  async me(@Req() request: any) {
+    return request.userId;
+  }
 }
