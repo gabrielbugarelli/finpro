60152b84e7ad2ae0cbe6e297032294ce6f3e3527
gabrielbugarelli <gabrielfonsecaoficial@gmail.com>
2023-11-02 10:25:24 -0400

    refactor[backend]: recovering user id with decorator param

--- apps/backend/src/modules/users/users.controller.ts
+++ apps/backend/src/modules/users/users.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get, Req } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { ActiveUserId } from 'src/shared/decorators/ActiveUserId';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get('me')
-  async me(@Req() request: any): Promise<{ name: string; email: string }> {
-    return this.usersService.getUserById(request.userId);
+  async me(
+    @ActiveUserId() userId: string,
+  ): Promise<{ name: string; email: string }> {
+    return await this.usersService.getUserById(userId);
   }
 }
