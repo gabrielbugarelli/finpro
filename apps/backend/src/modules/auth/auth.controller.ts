b13f5b00ffd3adea88293d2991779eed81af06bb
gabrielbugarelli <gabrielfonsecaoficial@gmail.com>
2023-11-02 07:05:13 -0400

    feat[backend]: custom decorator for verify route token

--- apps/backend/src/modules/auth/auth.controller.ts
+++ apps/backend/src/modules/auth/auth.controller.ts
@@ -1,19 +1,19 @@
-import { Body, Controller, Post, SetMetadata } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignInDTO } from './dtos/sign-in.dto';
 import { SignUpDTO } from './dtos/sign-up.dto';
+import { IsPublic } from 'src/shared/decorators/isPublic';
 
+@IsPublic()
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('signin')
-  @SetMetadata('IS_PUBLIC', true)
   async signIn(@Body() signInDTO: SignInDTO): Promise<{ accessToken: string }> {
     return await this.authService.signIn(signInDTO);
   }
 
-  @SetMetadata('IS_PUBLIC', true)
   @Post('signup')
   async signUp(@Body() signUpDTO: SignUpDTO): Promise<{ accessToken: string }> {
     return await this.authService.signUp(signUpDTO);
